70d06b477cca77c210c368a02bd27deb156633e0
scorchedrice <wldnd2977@gmail.com>
2025-06-18 14:54:07 +0900

    fix: dist conflict error

--- dist/apis/applies/dto/create-apply.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateApplyDto = void 0;
-class CreateApplyDto {
-}
-exports.CreateApplyDto = CreateApplyDto;
-//# sourceMappingURL=create-apply.dto.js.map
