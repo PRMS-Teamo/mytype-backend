759305dac04e3d97f02783836798fe83dc2340b9
김종현 <jhk9027@gmail.com>
2025-07-07 18:22:06 +0900

    fix: jwt 유저 필드 이름

--- src/apis/auth/strategies/jwt.strategy.ts
+++ src/apis/auth/strategies/jwt.strategy.ts
@@ -6,20 +6,15 @@ import { ConfigService } from "@nestjs/config";
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
   constructor(private configService: ConfigService) {
-    const jwtSecret = configService.get("JWT_SECRET");
-    if (!jwtSecret) {
-      throw new Error("JWT_SECRET is not defined");
-    }
-
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: jwtSecret,
+      secretOrKey: configService.get("JWT_SECRET") as string,
     });
   }
 
   validate(payload: any) {
     return {
-      userId: payload.userId || payload.sub,
+      user_id: payload.userId,
     };
   }
 }
