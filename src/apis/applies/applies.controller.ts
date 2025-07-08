2c40268db205589495ac8690ea9922f6c82af22b
scorchedrice <wldnd2977@gmail.com>
2025-07-08 17:16:48 +0900

    refactor: @Res 활용 응답

--- src/apis/applies/applies.controller.ts
+++ src/apis/applies/applies.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Body, Put, UseGuards, Request } from "@nestjs/common";
+import { Controller, Body, Put, UseGuards, Req, Res } from "@nestjs/common";
 import { AppliesService } from "./applies.service";
 import { UpsertApplyRequestDto } from "./dto/upsert-apply.request.dto";
 import { AccessTokenGuard } from "../auth/guard/bearer-token.guard";
 import { User } from "@/apis/auth/types/auth.interface";
+import { Response, Request } from "express";
 
 @Controller("applies")
 export class AppliesController {
@@ -10,13 +11,17 @@ export class AppliesController {
 
   @UseGuards(AccessTokenGuard)
   @Put()
-  upsert(
+  async upsert(
     @Body() upsertApplyRequestDto: UpsertApplyRequestDto,
-    @Request() req: any,
+    @Req() req: Request,
+    @Res() res: Response,
   ) {
     const user = req.user as User;
     const userId = user.userId;
     upsertApplyRequestDto.user_id = userId;
-    return this.appliesService.upsert(upsertApplyRequestDto);
+    const upsertApply = await this.appliesService.upsert(upsertApplyRequestDto);
+    return res
+      .status(201)
+      .send({ message: "지원이 정상적으로 완료되었습니다." });
   }
 }
