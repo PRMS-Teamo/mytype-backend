9eeb7c347b9b435e08013bbf20b1df2462802011
김종현 <jhk9027@gmail.com>
2025-07-09 10:44:06 +0900

    fix: teams: revert router

--- src/apis/teams/teams.controller.ts
+++ src/apis/teams/teams.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Post, Body, UseGuards, Req, Res } from "@nestjs/common";
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Req,
+  Res,
+  Param,
+  Patch,
+} from "@nestjs/common";
 import { TeamsService } from "./teams.service";
 import { CreateTeamDto } from "./dto/create-team.dto";
 import { AccessTokenGuard } from "@/apis/auth/guard/bearer-token.guard";
 import { Request, Response } from "express";
 import { User } from "@/apis/auth/types/auth.interface";
 import { UsersService } from "@/apis/users/users.service";
+import { UpdateTeamDto } from "./dto/update-team.dto";
 
 @Controller("teams")
 export class TeamsController {
@@ -26,5 +36,18 @@ export class TeamsController {
     const userId = userInfo.user_id;
     const response = await this.teamsService.createTeam(userId, createTeamDto);
     return res.status(201).send(response);
+  }
+
+  @Patch(":id")
+  @UseGuards(AccessTokenGuard)
+  async patchTeam(
+    @Param("id") id: string,
+    @Body() updateTeamDto: UpdateTeamDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const user = req.user as User;
+    await this.teamsService.updateTeam(user.userId, id, updateTeamDto);
+    return res.status(201).send({ message: "팀 정보 업데이트 성공" });
   }
 }
